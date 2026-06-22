af8cecaf92661f272f7f904affe7a45e57a509d3
Dinar <dev7@sigmasms.ru>
2026-06-22 10:28:03 +0500

    add config

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports.baseURL = 'http://localhost:3002';
+// module.exports.baseURL = 'https://api.work-assistant.ru';
+
+module.exports.ORG = 1;
+
+module.exports.dadataToken = 'b5e8642fbb95d59abcec80b98f39df95afa2977b';
+
+module.exports.businessFormName = 'KomboMarketData';
